aecc0a44772c0ceaf103bc18b96b1972ddf649e8
Ardian <ardian11@ui.ac.id>
2023-05-23 20:04:38 +0700

    Tutorial 8: Revisi

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
